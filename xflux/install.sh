968e42ecc47f7b44a0f62c393bec58585ec0b1e8
raulc <raci0399@gmail.com>
2024-09-09 19:54:20 +0200

    install dependencies

--- xflux/install.sh
+++ xflux/install.sh
@@ -2,9 +2,7 @@
 
 git clone https://github.com/XLabs-AI/x-flux.git
 
-cd x-flux
-
 python3 -m venv xflux_env
 source xflux_env/bin/activate
 
-pip install -r requirements.txt
+pip install -r x-flux/requirements.txt
