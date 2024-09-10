6a1340d491323fd567562b16588e300e30fc7cd7
raulc <raci0399@gmail.com>
2024-09-10 11:24:48 +0200

    run fp8

---
+++ xflux/run_fp8.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PROMPT='The image depicts a modern, minimalist two-story residential building with a white exterior. Its cuboid shape features clean lines and sharp angles, creating a sleek look.
+Large rectangular windows with dark frames punctuate both floors, some illuminated from within. A small balcony with thin black metal railings extends from the second floor. An external black metal staircase leads to the upper entrance, adding visual interest.
+The building is part of a uniform row of similar structures on a gentle slope, ensuring unobstructed views for each unit. The scene is captured during golden hour, with warm light casting subtle shadows that accentuate the geometric forms and give the white exterior a slight cream tint in places.
+Well-maintained landscaping, including a manicured lawn with wildflowers and ornamental grasses, softens the stark architecture and integrates it with the natural surroundings.'
+
+output_path="../imgs/xflux.png"
+
+python3 x-flux/main.py \
+ --prompt "$PROMPT" \
+ --offload --model_type flux-dev-fp8 \
+ --lora_repo_id XLabs-AI/flux-lora-collection --lora_name realism_lora.safetensors \
+ --guidance 4 \
+ --save_path "$output_path"
