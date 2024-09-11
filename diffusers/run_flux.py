212062bfe162411a70781e02dc475cc7d3d1d5db
raulc <raci0399@gmail.com>
2024-09-11 11:27:01 +0200

    run flux on 24GB

--- diffusers/run_flux.py
+++ diffusers/run_flux.py
@@ -7,15 +7,15 @@ Large rectangular windows with dark frames punctuate both floors, some illuminat
 The building is part of a uniform row of similar structures on a gentle slope, ensuring unobstructed views for each unit. The scene is captured during golden hour, with warm light casting subtle shadows that accentuate the geometric forms and give the white exterior a slight cream tint in places.
 Well-maintained landscaping, including a manicured lawn with wildflowers and ornamental grasses, softens the stark architecture and integrates it with the natural surroundings."""
 
-pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-dev").to('cuda')
-# pipe.enable_model_cpu_offload()
+pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", torch_dtype=torch.bfloat16)
+pipe.enable_model_cpu_offload()
 
 out = pipe(
     prompt=prompt,
     guidance_scale=3.5,
     height=768,
     width=1360,
-    num_inference_steps=50,
+    num_inference_steps=30,
 ).images[0]
 
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
