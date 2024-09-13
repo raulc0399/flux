6a465602790e87d422b23862971bdd68b601a069
raci0399 <raci0399@gmail.com>
2024-09-13 10:29:26 +0200

    multi gpu

--- diffusers/run_shakkerlabs.py
+++ diffusers/run_shakkerlabs.py
@@ -1,9 +1,9 @@
 import torch
 from diffusers.utils import load_image
-
 from diffusers import FluxControlNetPipeline, FluxControlNetModel
 from diffusers.models import FluxMultiControlNetModel
 from datetime import datetime
+from accelerate.utils import compute_module_sizes
 
 control_image_canny = load_image("../images/ctrl1024.jpg")
 control_mode_canny = 0
@@ -16,11 +16,22 @@ Well-maintained landscaping, including a manicured lawn with wildflowers and orn
 base_model = 'black-forest-labs/FLUX.1-dev'
 controlnet_model_union = 'Shakker-Labs/FLUX.1-dev-ControlNet-Union-Pro'
 
-controlnet_union = FluxControlNetModel.from_pretrained(controlnet_model_union, torch_dtype=torch.bfloat16)
+controlnet_union = FluxControlNetModel.from_pretrained(controlnet_model_union, torch_dtype=torch.bfloat16).to("cuda:1")
 controlnet = FluxMultiControlNetModel([controlnet_union]) # we always recommend loading via FluxMultiControlNetModel
 
-pipe = FluxControlNetPipeline.from_pretrained(base_model, controlnet=controlnet, torch_dtype=torch.bfloat16)
-pipe.to("cuda")
+pipe = FluxControlNetPipeline.from_pretrained(
+    base_model, controlnet=controlnet, torch_dtype=torch.bfloat16, device_map="balanced"
+)
+
+# pipe.load_lora_weights("XLabs-AI/flux-RealismLora")
+# pipe.fuse_lora(lora_scale=1.1)
+
+print(pipe.hf_device_map)
+print(controlnet.device);
+print(compute_module_sizes(controlnet, dtype=torch.bfloat16)[""])
+
+print(controlnet_union.device);
+print(compute_module_sizes(controlnet_union, dtype=torch.bfloat16)[""])
 
 width, height = control_mode_canny.size
 
@@ -33,7 +44,7 @@ image = pipe(
     controlnet_conditioning_scale=[1.0],
     num_inference_steps=30, 
     guidance_scale=4,
-    generator=torch.manual_seed(42),
+    # generator=torch.manual_seed(42),
 ).images[0]
 
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
