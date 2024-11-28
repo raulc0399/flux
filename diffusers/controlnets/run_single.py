c415eb08e585d2982ffd30630e075c4c0441d48a
raci0399 <raci0399@gmail.com>
2024-11-28 11:34:15 +0100

    info about the model

--- diffusers/controlnets/run_single.py
+++ diffusers/controlnets/run_single.py
@@ -241,6 +241,8 @@ def main(model_index):
         model_name = model.replace("/", "-")
         ensure_params_dir(model_name)
 
+        print(f"Generating images for model: {model}")
+
         control_image_paths = get_control_images(model)
 
         pipe = load_pipeline(model)
