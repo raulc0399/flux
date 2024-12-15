f31bb5971465541b569a549dd8039a7d91583930
raulc0399 <raci0399@gmail.com>
2024-12-15 21:31:56 +0100

    try lora

--- diffusers/nf4_run.py
+++ diffusers/nf4_run.py
@@ -17,7 +17,7 @@ ckpt_id = "black-forest-labs/FLUX.1-dev"
 pipeline = FluxPipeline.from_pretrained(ckpt_id, text_encoder_2=text_encoder_2_4bit, transformer=None, vae=None, torch_dtype=torch.float16)
 pipeline.enable_model_cpu_offload()
 
-prompt = "a cute dog in paris photoshoot"
+prompt = "armina woman running on a beatufil beach, looking at the camera"
 with torch.no_grad():
     print("Encoding prompts.")
     prompt_embeds, pooled_prompt_embeds, text_ids = pipeline.encode_prompt(prompt=prompt, prompt_2=None, max_sequence_length=256)
@@ -29,7 +29,14 @@ with torch.no_grad():
 transformer_4bit = FluxTransformer2DModel.from_pretrained(ckpt_4bit_id, subfolder="transformer")
 pipeline = FluxPipeline.from_pretrained(ckpt_id, text_encoder=None, text_encoder_2=None, tokenizer=None, tokenizer_2=None, transformer=transformer_4bit, torch_dtype=torch.float16)
 pipeline.enable_model_cpu_offload()
-                                                          
+
+print("Loading LoRA weights.")
+pipeline.load_lora_weights("./1.safetensors")
+# print("Fusing LoRA.")
+# pipeline.fuse_lora()
+# print("Unloading LoRA weights.")
+# pipeline.unload_lora_weights()
+
 print("Running denoising.")
 height, width = 512, 768
 images = pipeline(prompt_embeds=prompt_embeds, pooled_prompt_embeds=pooled_prompt_embeds, num_inference_steps=50, guidance_scale=5.5, height=height, width=width, output_type="pil").images
