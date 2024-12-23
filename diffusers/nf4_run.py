7258f1925a6c22605fbb028e19038935de25ea29
raulc0399 <raci0399@gmail.com>
2024-12-23 06:32:52 +0100

    running nf4 pipeline

--- diffusers/nf4_run.py
+++ diffusers/nf4_run.py
@@ -12,37 +12,28 @@ def flush():
     torch.cuda.reset_peak_memory_stats()
 
 ckpt_4bit_id = "sayakpaul/flux.1-dev-nf4-pkg"
-text_encoder_2_4bit = T5EncoderModel.from_pretrained(ckpt_4bit_id, subfolder="text_encoder_2")
-
 ckpt_id = "black-forest-labs/FLUX.1-dev"
-pipeline = FluxPipeline.from_pretrained(ckpt_id, text_encoder_2=text_encoder_2_4bit, transformer=None, vae=None, torch_dtype=torch.float16)
-pipeline.enable_model_cpu_offload()
 
-prompt = "armina woman running on a beatufil beach, looking at the camera"
-with torch.no_grad():
-    print("Encoding prompts.")
-    prompt_embeds, pooled_prompt_embeds, text_ids = pipeline.encode_prompt(prompt=prompt, prompt_2=None, max_sequence_length=256)
-    pipeline = pipeline.to("cpu")
-    del pipeline
-    
-    flush()
-
+text_encoder_2_4bit = T5EncoderModel.from_pretrained(ckpt_4bit_id, subfolder="text_encoder_2")
 transformer_4bit = FluxTransformer2DModel.from_pretrained(ckpt_4bit_id, subfolder="transformer")
-pipeline = FluxPipeline.from_pretrained(ckpt_id, text_encoder=None, text_encoder_2=None, tokenizer=None, tokenizer_2=None, transformer=transformer_4bit, torch_dtype=torch.float16)
+
+pipeline = FluxPipeline.from_pretrained(ckpt_id, text_encoder_2=text_encoder_2_4bit, transformer=transformer_4bit, torch_dtype=torch.float16)
 pipeline.enable_model_cpu_offload()
 
 print("Loading LoRA weights.")
 pipeline.load_lora_weights("./loras/009.safetensors")
-print("Fusing LoRA.")
-pipeline.fuse_lora()
+# print("Fusing LoRA.")
+# pipeline.fuse_lora()
 # print("Unloading LoRA weights.")
 # pipeline.unload_lora_weights()
 
+prompt = "cartoon of woman running on a beautiful beach, looking at the camera"
 print("Running denoising.")
 height, width = 512, 768
-images = pipeline(prompt_embeds=prompt_embeds, pooled_prompt_embeds=pooled_prompt_embeds, 
-                  num_inference_steps=50, guidance_scale=5.5,
-                  height=height, width=width, output_type="pil").images
+with torch.no_grad():
+    images = pipeline(prompt=prompt, 
+                      num_inference_steps=50, guidance_scale=5.5,
+                      height=height, width=width, output_type="pil").images
 
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 images[0].save(f"./output/{timestamp}_output.png")
